1d578620373a46cac5929a0237912756f9188a34
crueter <crueter@eden-emu.dev>
2026-01-08 15:14:02 -0500

    fix openssl
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/openssl.sh
+++ tools/openssl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 export OPENSSL_VERSION=3.6.0-1cb0d36b39
-export OPENSSL_DIR="$ROOTDIR"/openssl
+export OPENSSL_DIR="$ROOTDIR/openssl-$PLATFORM-$ARCH-$VERSION"
 
 _download="https://github.com/crueter-ci/OpenSSL/releases/download/v$OPENSSL_VERSION/openssl-$PLATFORM-$ARCH-$OPENSSL_VERSION.tar.zst"
 _artifact="openssl-$PLATFORM-$ARCH-$OPENSSL_VERSION.tar.zst"
@@ -10,12 +10,12 @@ download_openssl() {
 	if [ ! -d "$OPENSSL_DIR" ]; then
 		[ -f "$_artifact" ] || curl -L "$_download" -o "$_artifact"
 		mkdir -p "$OPENSSL_DIR"
-		tar xf "$_artifact" -C "$OPENSSL_DIR"
+		$TAR xf "$_artifact" -C "$OPENSSL_DIR"
 		rm -f "$OPENSSL_DIR"/CMakeLists.txt "$OPENSSL_DIR"/lib/*.so* "$OPENSSL_DIR"/bin/*.dll*
 	fi
 
 	find "$OPENSSL_DIR" -name "*.pc" | while read -r pc; do
-		sed "s|prefix=\/$|prefix=$OPENSSL_DIR|g" "$pc" > "$pc".tmp
+		sed "s|^prefix=\/.*$|prefix=$OPENSSL_DIR|g" "$pc" > "$pc".tmp
 		mv "$pc".tmp "$pc"
 	done
 }
