6d545075484c2d6bee049ffe495dc9d1ef79a0a6
crueter <crueter@eden-emu.dev>
2026-01-08 16:42:49 -0500

    fuck you
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/common.sh
+++ tools/common.sh
@@ -72,8 +72,7 @@ extract() {
 	find . -type f -name "*.c" | while read -r file; do
 		sed 's/if HAVE_UNISTD_H/if HAVE_UNISTD_H || defined(__APPLE__)/' "$file"   > "$file".1
 		sed 's/if HAVE_DIRENT_H/if HAVE_DIRENT_H || defined(__APPLE__)/' "$file".1 > "$file".2
-		sed 's/if HAVE_IO_H/if HAVE_IO_H || defined(__APPLE__)/'         "$file".2 > "$file".3
-		mv "$file".3 "$file"
+		mv "$file".2 "$file"
 	done
 
 	popd
