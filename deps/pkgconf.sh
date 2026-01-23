65ef285c7bdc6da4da0e65e544366536a26d069f
crueter <crueter@eden-emu.dev>
2026-01-23 15:43:50 -0500

    add zstd
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- deps/pkgconf.sh
+++ deps/pkgconf.sh
@@ -8,10 +8,11 @@ _dir="$ROOTDIR/$_name-$ARCH-$_version"
 _download="https://github.com/crueter-ci/$_repo/releases/download/v$_version/$_name-$ARCH.exe"
 _artifact="pkg-config.exe"
 
-if ! command -v pkg-config >/dev/null 2>&1; then
-	[ -d "$_dir" ] || mkdir "$_dir"
+# have to skip cmd check here because perl overrides it with its broken pkg-config
+# if ! command -v pkg-config >/dev/null 2>&1; then
+[ -d "$_dir" ] || mkdir "$_dir"
 
-	[ -f "$_dir/$_artifact" ] || { echo "-- Downloading pkgconf..."; curl -L "$_download" -o "$_dir/$_artifact"; }
+[ -f "$_dir/$_artifact" ] || { echo "-- Downloading pkgconf..."; curl -L "$_download" -o "$_dir/$_artifact"; }
 
-	export PATH="$PATH:$_dir"
-fi
+export PATH="$_dir:$PATH"
+# fi
